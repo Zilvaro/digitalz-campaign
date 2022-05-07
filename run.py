089d7ae41eb5b1a5820c423ed581c23dfd8f50bc
Zilvaro <zilvinas@digitalcase.ch>
2022-05-07 07:58:50 +0000

    Input fields texts design

--- run.py
+++ run.py
@@ -23,7 +23,7 @@ def title():
     os.system("cls" if os.name == "nt" else "clear")
     print("\033[1;36m")
     print("  WINTER-CAMPAIGN SUMMARY  ".center(90, "-"))
-    
+        
 
 title()
 
@@ -32,7 +32,6 @@ title()
 report1 = "Number of participants every day by region"
 report2 = "Time of answering questions during the day"
 report3 = "Answer by region to different type of questions"
-screen_filler = 90
 
 
 def welcome():
@@ -48,14 +47,15 @@ def welcome():
     print("\033[0;37m  And we were observing their answering Behavior ")
     print("\033[0;36m  Please, select the report and dates for the analysis:\n")
 
-    print(f"\033[0;36m    # 1 :\033[0;37m  {report1}")
-    print(f"\033[0;36m    # 2 :\033[0;37m  Time of answering questions during the day")
-    print(f"\033[0;36m    # 3 :\033[0;37m  Answer by region to different type of questions\n")
+    print("\033[0;36m    # 1 :\033[0;37m  {report1}")
+    print("\033[0;36m    # 2 :\033[0;37m  Time of answering questions during the day")
+    print("\033[0;36m    # 3 :\033[0;37m  Answer by region to different type of questions\n")
 
-    print(f"\033[0;36m    Any period between 1st and 31st of the month : \033[0;37m  Dates for analysis\n")
+    print("\033[0;36m    Any period between 1st and 31st of the month : \033[0;37m  Dates for analysis\n")
     print("-".center(90, "-"))
     print("\n\n")
      
+
 welcome()
 
 
@@ -66,37 +66,35 @@ def get_input_data():
     """
     # Input & check the report number
     report_number = int(input("Enter the report number here (1 to 3): "))
-    if report_number < 1 or report_number > 3:
+    if report_number < 1 or report_number > 3: 
         report_number = int(input(
-            "\n\033[1;31mInvalid input, Please type a number between 1 & 3:\033[0;37m\n"
+            "\n\033[1;31mInvalid input, Please type a number between \033[0;37m1 & 3: \n"
         ))
+    
 
     # Input & check the period dates
     start_date = int(input("\nEnter the NUMBER of start-day (1 to 31): "))
     if start_date < 1 or start_date > 31:
         start_date = int(input(
-            "\n\033[1;31mInvalid input, Please type a number between 1 & 31:\033[0;37m\n"
+            "\n\033[1;31mInvalid input, Please type a number between \033[0;37m1 & 31:\n"
         ))
 
     end_date = int(input("\nEnter the NUMBER of end-day (1 to 31): "))
     if (end_date <1 or end_date >31) or (start_date > end_date):
         end_date = int(input(
-            f"\n\033[1;31mInvalid input, Please type a number between {start_date} and 31:\033[0;37m\n"
+            f"\n\033[1;31mInvalid input, Please type a number between \033[0;37m{start_date} and 31:\n"
         ))
 
     # Print the user selected options 
     if report_number == 1:
-        print("\033[0;37mYou selected:")
-        print(
-            f"'\033[1;36m {report1}' \033[1;37mfrom {start_date} to {end_date} of December".center(screen_filler,))
-    
+        print("\n\n\033[0;37mYou selected:\n")
+        print(f"\n\033[1;36m '{report1}' \033[1;37mfrom {start_date} to {end_date} of December")
     elif report_number == 2:
-        print(
-            f"You selected: '\033[0;37m {report2}' from {start_date} to {end_date} of December")
-
-    else :
-        print(
-            f"You selected: '\033[0;37m {report3}' from {start_date} to {end_date} of December\n\n")
+        print("\n\n\033[0;37mYou selected:\n")
+        print(f"\n\033[1;36m '{report2}' \033[1;37mfrom {start_date} to {end_date} of December")   
+    else:
+        print("\n\n\033[0;37mYou selected:\n")
+        print(f"\n\033[1;36m '{report3}' \033[1;37mfrom {start_date} to {end_date} of December")
 
 
 get_input_data()
