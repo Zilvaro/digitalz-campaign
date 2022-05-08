a32f921f90b60fb3a7c4633389f5b6b3bfe98493
Zilvaro <zilvinas@digitalcase.ch>
2022-05-08 03:45:01 +0000

    Constant variables UPPERCASED and updated in code

--- run.py
+++ run.py
@@ -29,9 +29,9 @@ title()
 
 
 # Names & numbers of reports plus global variables
-report1 = "Number of participants every day by region"
-report2 = "Time of answering questions during the day"
-report3 = "Answer by region to different type of questions"
+REPORT1 = "Number of participants every day by region"
+REPORT2 = "Time of answering questions during the day"
+REPORT3 = "Answer by region to different type of questions"
 
 
 def welcome():
@@ -40,16 +40,16 @@ def welcome():
     print("\033[0;37m")
     print(" Welcome to data and insights of our last Christmas campaign!\n\n".center(90,))
     print(" During December, every day we were sending to shops business"
-        " or research tasks, e.g:")
-    print(f"\033[0;34m    Business: \033[0;37m 'Show the availability of FROZEN NUGGETS'")
-    print(f"\033[0;34m    Research: \033[0;37m '% of sales increase due to this promo?'")
+          " or research tasks, e.g:")
+    print("\033[0;34m    Business: \033[0;37m 'Show the availability of FROZEN NUGGETS'")
+    print("\033[0;34m    Research: \033[0;37m '% of sales increase due to this promo?'")
     print("\n")
     print("\033[0;37m  And we were observing their answering Behavior ")
     print("\033[0;36m  Please, select the report and dates for the analysis:\n")
 
-    print("\033[0;36m    # 1 :\033[0;37m  {report1}")
-    print("\033[0;36m    # 2 :\033[0;37m  Time of answering questions during the day")
-    print("\033[0;36m    # 3 :\033[0;37m  Answer by region to different type of questions\n")
+    print(f"\033[0;36m    # 1 :\033[0;37m  {REPORT1}")
+    print(f"\033[0;36m    # 2 :\033[0;37m  {REPORT2}")
+    print(f"\033[0;36m    # 3 :\033[0;37m  {REPORT3}\n")
 
     print("\033[0;36m    Any period between 1st and 31st of the month : \033[0;37m  Dates for analysis\n")
     print("-".center(90, "-"))
@@ -65,41 +65,43 @@ def get_input_data():
     """
     user_input = SHEET.worksheet('data-input')
 
-    # Input & check the report number
+    # Input, check the report number and update google sheet
     report_number = int(input("Enter the report number here (1 to 3): "))
     while report_number < 1 or report_number > 3: 
         report_number = int(input(
             "\n\033[1;31mInvalid input, Please type a number between \033[0;37m1 & 3: \n"
         ))
-    report_value = user_input.update ("A2", report_number)
+
+    user_input.update("A2", report_number)
     
-    # Input & check the period dates
+    # Input, check the period dates & update the google sheet
     start_date = int(input("\nEnter the NUMBER of start-day (1 to 31): "))
     while start_date < 1 or start_date > 31:
         start_date = int(input(
             "\n\033[1;31mInvalid input, Please type a number between \033[0;37m1 & 31:\n"
         ))
 
-    start_value = user_input.update ("B2", start_date)
+    user_input.update("B2", start_date)
 
-    end_date = int(input("\nEnter the NUMBER of end-day (1 to 31): "))
-    while (end_date <1 or end_date >31) or (start_date > end_date):
+    end_date = int(input(f"\nEnter the NUMBER of end-day ({start_date} to 31): "))
+    while (end_date < 1 or end_date > 31) or (start_date > end_date):
         end_date = int(input(
             f"\n\033[1;31mInvalid input, Please type a number between \033[0;37m{start_date} and 31:\n"
         ))
 
-    end_value = user_input.update ("C2", end_date)
+    user_input.update("C2", end_date)
+
 
     # Print the user selected options 
     if report_number == 1:
         print("\n\n\033[0;37mYou selected:\n")
-        print(f"\n\033[1;36m '{report1}' \033[1;37mfrom {start_date} to {end_date} of December")
+        print(f"\n\033[1;36m '{REPORT1}' \033[1;37mfrom {start_date} to {end_date} of December")
     elif report_number == 2:
         print("\n\n\033[0;37mYou selected:\n")
-        print(f"\n\033[1;36m '{report2}' \033[1;37mfrom {start_date} to {end_date} of December")
+        print(f"\n\033[1;36m '{REPORT2}' \033[1;37mfrom {start_date} to {end_date} of December")
     else:
         print("\n\n\033[0;37mYou selected:\n")
-        print(f"\n\033[1;36m '{report3}' \033[1;37mfrom {start_date} to {end_date} of December")
+        print(f"\n\033[1;36m '{REPORT3}' \033[1;37mfrom {start_date} to {end_date} of December")
 
 
 get_input_data()
